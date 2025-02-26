7d88c1fc1ef5158a87b794c653710249a10d75cc
Filipp Pavlenko <filipp02@mail.ru>
2025-02-26 16:40:55 +0300

    added sum func

--- app.js
+++ app.js
@@ -1,3 +1,7 @@
-function hello(){
-    console.log('Hello world');
+function hello() {
+  console.log("Hello world");
+}
+function sumOfArray(arr) {
+  let sum = arr.reduce((a, b) => a + b);
+  return sum;
 }
